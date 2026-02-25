5f74669eaac81d8e6bf813c48d06d20cb46d4ea6
cincarnato <cristian.incarnato@sondeos.com.ar>
2026-02-25 14:25:29 -0300

    fix: remove graphql and sqlite deps

--- docker-build-snd.sh
+++ docker-build-snd.sh
@@ -1,2 +1,2 @@
-docker build -t sndregistry.sondeosglobal.com/inca/something:1.0.2 .
-docker push  sndregistry.sondeosglobal.com/inca/something:1.0.2
+docker build -t sndregistry.sondeosglobal.com/ai/agent-studio:1.0.0 .
+docker push  sndregistry.sondeosglobal.com/ai/agent-studio:1.0.0
